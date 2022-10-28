fa127a59b3909b9aa0e5b280d5a8109a3862fd01
Dylan Palmquist <111770130+dmpalmquist@users.noreply.github.com>
2022-10-28 15:40:01 +0000

    the for loop worked

--- .ccls-cache/@home@runner@Homework-10-Problem-4/main.cpp
+++ .ccls-cache/@home@runner@Homework-10-Problem-4/main.cpp
@@ -10,7 +10,9 @@ int main() {
   int total = 0, *nPt; 
 
   nPt = Nums;
-  while (nPt < Nums + nums)
+  //while (nPt < Nums + nums)
+    //total += *nPt++;
+  for (int i = 0; i < nums; i++)
     total += *nPt++;
 
   cout << "The total of the array elements is " << total << endl;
